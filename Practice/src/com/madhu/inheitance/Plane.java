66f7993ee586022ceca2d50c84ab780dfbc87a56
madhu <madhu.java.git@gmail.com>
2022-11-16 12:32:45 +0530

    basic inheritance

--- Practice/src/com/madhu/inheitance/Plane.java
+++ Practice/src/com/madhu/inheitance/Plane.java
@@ -1,10 +1,56 @@
 package com.madhu.inheitance;
 
-public class Plane {
+class Plane {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void takeOff() {
+		System.out.println("plane is taking off");
 	}
 
+	public void fly() {
+		System.out.println("plane is flying");
+	}
+
+	public void land() {
+		System.out.println("plane is landing");
+	}
+	public static void main(String[] args) {
+		CargoPlane cp= new CargoPlane();
+		PassengerPlane pp = new PassengerPlane();
+		cp.takeOff();
+		cp.fly();
+		cp.land();
+		pp.takeOff();
+		pp.fly();
+		pp.land();
+		
+		}
+}
+class CargoPlane extends Plane{
+	public void fly() {
+		System.out.println("cargo plane flies at lower height");
+	}
+	public void cargoGoods() {
+		System.out.println("cargo plane carries goods");
+	}
+}
+class PassengerPlane extends Plane{
+	public void fly() {
+		System.out.println("passenger plane flies at medium height");
+	}
+	public void cargoGoods() {
+		System.out.println("passenger plane carries passengers");
+	}
+}
+class LaunchPlane{
+//	public static void main(String[] args) {
+//		CargoPlane cp= new CargoPlane();
+//		PassengerPlane pp = new PassengerPlane();
+//		cp.takeOff();
+//		cp.fly();
+//		cp.land();
+//		pp.takeOff();
+//		pp.fly();
+//		pp.land();
+//		
+//		}
 }
