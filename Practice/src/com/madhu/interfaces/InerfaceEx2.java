6a9546767556d85099a3d14714fdc354575de7ee
madhu <madhu.java.git@gmail.com>
2022-11-23 12:46:01 +0530

    methods with same name but diff params

--- Practice/src/com/madhu/interfaces/InerfaceEx2.java
+++ Practice/src/com/madhu/interfaces/InerfaceEx2.java
@@ -3,11 +3,13 @@ package com.madhu.interfaces;
 interface IDemo1{
 	public abstract void m1();
 	void m2();
+	void m4(int a, int b);
 	}
 interface IDemo2{
 	public abstract void m1();
 	//public abstract int m1();//not valid
 	void m3();
+	void m4(int a, String b);
 	}
 public class InerfaceEx2 implements IDemo1,IDemo2 {
 
@@ -24,6 +26,12 @@ public class InerfaceEx2 implements IDemo1,IDemo2 {
 	public void m3() {
 		System.out.println("i am from idemo2");
 	}
+	public void m4(int a , int b) {
+		System.out.println(a*b);
+	}
+	public void m4(int a ,String b) {
+		System.out.println(a+b);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		IDemo1 demo1 = new InerfaceEx2();
@@ -32,6 +40,8 @@ public class InerfaceEx2 implements IDemo1,IDemo2 {
 		IDemo2 demo2 = new InerfaceEx2();
 		demo2.m1();
 		demo2.m3();
+		demo1.m4(10, 10);
+		demo2.m4(10, "ten");
 
 	}
 
