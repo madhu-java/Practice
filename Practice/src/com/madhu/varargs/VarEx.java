d83515c3075e275f45f3442dbee97c6f3cf6ec42
madhu <madhu.java.git@gmail.com>
2022-11-29 04:59:08 +0530

    with varargs

--- Practice/src/com/madhu/varargs/VarEx.java
+++ Practice/src/com/madhu/varargs/VarEx.java
@@ -1,17 +1,25 @@
 package com.madhu.varargs;
 class Vars{
-	public void add() {
-		System.out.println("nothing to add");
-	}
-	public void add (int a ) {
-		System.out.println(a);
-	}
-	public void add (int a , int b) {
-		System.out.println(a+b);
-	}
-	public void add (int a , int b,int c) {
-		System.out.println(a+b
-				+c);
+//	public void add() {
+//		System.out.println("nothing to add");
+//	}
+//	public void add (int a ) {
+//		System.out.println(a);
+//	}
+//	public void add (int a , int b) {
+//		System.out.println(a+b);
+//	}
+//	public void add (int a , int b,int c) {
+//		System.out.println(a+b
+//				+c);
+//	}
+	public void add(int...a) {
+		System.out.println(a.length);
+		int total=0;
+		for(int i : a) {
+			total+= i;
+		}
+		System.out.println(total);
 	}
 }
 public class VarEx {
