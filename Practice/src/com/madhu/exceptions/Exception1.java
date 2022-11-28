d82380dbbb595730666bb775ad1d3c75e9964a6d
madhu <madhu.java.git@gmail.com>
2022-11-28 21:13:55 +0530

    rethrowing

--- Practice/src/com/madhu/exceptions/Exception1.java
+++ Practice/src/com/madhu/exceptions/Exception1.java
@@ -1,7 +1,9 @@
 package com.madhu.exceptions;
 
+import javax.sound.midi.Soundbank;
+
 public class Exception1 {
-	public void  divide(int a,int b) {
+	public void  divide(int a,int b)throws Exception {
 		try {
 		double res = a/b;
 		System.out.println( res);
@@ -12,6 +14,7 @@ public class Exception1 {
 		}
 		catch(Exception e) {
 			System.out.println(e);
+			throw e;
 		}
 //		catch(ArithmeticException e){//this catch is unreachable
 //			System.out.println(e);
@@ -20,11 +23,16 @@ public class Exception1 {
 		System.out.println("You have division successfully");
 		
 	}
+	
 
 	public static void main(String[] args) {
 		Exception1 exception1 =  new Exception1();
+		try {
 		exception1.divide(10,10);
 		exception1.divide(10,0);
+		}catch(Exception e) {
+			System.out.println("in main");
+		}
 
 	}
 
