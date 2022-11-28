8b11e7a5de78e80c7a7ddcc80755cbeaf810cdf7
madhu <madhu.java.git@gmail.com>
2022-11-29 05:10:57 +0530

    commit

--- Practice/src/com/madhu/varargs/VarEx.java
+++ Practice/src/com/madhu/varargs/VarEx.java
@@ -19,7 +19,26 @@ class Vars{
 		for(int i : a) {
 			total+= i;
 		}
-		System.out.println(total);
+		System.out.println("addition:"+total);
+	}
+//	public void add(int[] b) {
+//		
+//	}
+	public void add(int[]... a) {
+		int total =0;
+		for(int[] i :a) {
+			for(int j:i) {
+				total+=j;
+			}
+		}
+		System.out.println("addition:"+total);
+	}
+	public void multiply(int a, int ...b) {
+		int mul = a;
+		for(int i :b) {
+		mul*= i;
+	}
+		System.out.println("mul:"+mul);
 	}
 }
 public class VarEx {
@@ -27,10 +46,14 @@ public class VarEx {
 	public static void main(String[] args) {
 
 Vars vars = new Vars();
-vars.add();
+//vars.add();
 vars.add(10);
 vars.add(10,20);
 vars.add(10,20,30);
+vars.multiply(1,2,3,4,5,6);
+int[]a= {1,2,3,4};
+int[] b = {5,6,7,8,9};
+vars.add(a,b);
 
 	}
 
