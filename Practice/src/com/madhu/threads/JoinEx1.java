13fd3da88b8fa36e1704d6166ef2c7a05d5d52b2
madhu <madhu.java.git@gmail.com>
2022-12-06 18:49:38 +0530

    join ex

--- Practice/src/com/madhu/threads/JoinEx1.java
+++ Practice/src/com/madhu/threads/JoinEx1.java
@@ -4,6 +4,12 @@ class SClass extends Thread{
 	public void run() {
 		for(int i=1;i<=10;i++) {
 			sum+=i;
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
@@ -12,7 +18,9 @@ public class JoinEx1 {
 	public static void main(String[] args) throws InterruptedException {
 		SClass sClass = new SClass();
 		sClass.start();
-		
+		//sClass.join(2000);
+		//sClass.join();
+		sClass.join(2000,200);
 		System.out.println(sClass.sum);
 		System.out.println("main");
 	}
