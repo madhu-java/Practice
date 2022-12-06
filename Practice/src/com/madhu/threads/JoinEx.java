833cafd47a947a5f21f81357807582354ada64b3
madhu <madhu.java.git@gmail.com>
2022-12-06 17:43:36 +0530

    joinex

--- Practice/src/com/madhu/threads/JoinEx.java
+++ Practice/src/com/madhu/threads/JoinEx.java
@@ -14,9 +14,12 @@ class JClass extends Thread{
 }
 public class JoinEx {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		JClass jClass = new JClass();
+		
 		jClass.start();
+		System.out.println("main thread is waiting for Jclass thread to finish");
+		jClass.join();
 		for(int i=0;i<=5;i++) {
 			System.out.println("main:"+i);
 		}
