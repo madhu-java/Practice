19c3c78d9731ebe0b7e0e046cfff7e841d925135
madhu <madhu.java.git@gmail.com>
2022-12-10 07:27:02 +0530

    ArrayDequeEx

--- Practice/src/com/madhu/collections/ArrayDqueueEx.java
+++ Practice/src/com/madhu/collections/ArrayDqueueEx.java
@@ -14,6 +14,17 @@ public class ArrayDqueueEx {
 		arrayDeque.addFirst(2);
 		arrayDeque.addLast(1);
 		System.out.println(arrayDeque);
+		System.out.println(arrayDeque.contains(10));
+		System.out.println(arrayDeque.getFirst());
+		System.out.println(arrayDeque.getLast());
+		System.out.println(arrayDeque.size());
+		ArrayDeque a2 = new ArrayDeque();
+		a2.add("Madhu");
+		a2.add(true);
+		System.out.println(arrayDeque.containsAll(a2));
+		System.out.println(a2.isEmpty());
+		a2.clear();
+		System.out.println(a2);
 
 	}
 
