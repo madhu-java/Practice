4fc555f8416c88cc786ed4145516f496adb7d2db
madhu <madhu.java.git@gmail.com>
2022-12-15 19:27:18 +0530

    failsafeEx

--- Practice/src/com/madhu/collections/FailSafeEx.java
+++ Practice/src/com/madhu/collections/FailSafeEx.java
@@ -2,6 +2,7 @@ package com.madhu.collections;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class FailSafeEx {
 
@@ -16,11 +17,23 @@ public class FailSafeEx {
 //			System.out.println(aList.get(i));
 //			aList.add(0);
 //		}
+		//failfast
 		Iterator iterator=aList.iterator();
 		while(iterator.hasNext()) {
 			System.out.println(iterator.next());
-			aList.add(100);
+			//aList.add(100);
 		}
+		CopyOnWriteArrayList cop= new CopyOnWriteArrayList<>()
+;
+		cop.add(10);
+		cop.add(20);
+		cop.add(30);
+		Iterator iterator2=cop.iterator();
+	while(iterator2.hasNext()) {
+		System.out.println(iterator2.next());
+		cop.add(100);
 	}
+	System.out.println("we didn't get  any exception");
+		}
 
 }
