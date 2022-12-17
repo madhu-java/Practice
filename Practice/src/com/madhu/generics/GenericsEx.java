6a2179b3f06ff1dd8ba9c01845c1a2c11993d0ee
madhu <madhu.java.git@gmail.com>
2022-12-17 12:52:44 +0530

    collectionsneedtypecsting

--- Practice/src/com/madhu/generics/GenericsEx.java
+++ Practice/src/com/madhu/generics/GenericsEx.java
@@ -1,5 +1,7 @@
 package com.madhu.generics;
 
+import java.util.ArrayList;
+
 import javax.naming.ReferralException;
 
 public class GenericsEx {
@@ -10,9 +12,22 @@ public class GenericsEx {
 		arr[1]="bb";
 		arr[2]="cc";
 		//arr[3]=23;CE, bcos array is type safe
+		String s= arr[0];//no ty[ecstig required
+		System.out.println(s);
 
 		System.out.println(arr[0]);
 		System.out.println(arr[1]);
+		ArrayList aList= new ArrayList();
+		aList.add(10);
+		aList.add("aaa");
+		aList.add(10.09);
+		System.out.println();
+		//String string = aList.get(0); typecasting required
+		
+		String string = (String) aList.get(1);
+		int i = (Integer)aList.get(0);
+		System.out.println(string);
+		System.out.println(i);
 	}
 
 }
