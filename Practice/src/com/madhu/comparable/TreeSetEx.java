d9c1ba95118d2d8490d59fbe861132f835ac4690
madhu <madhu.java.git@gmail.com>
2022-12-25 21:40:10 +0530

    comparable example

--- Practice/src/com/madhu/comparable/TreeSetEx.java
+++ Practice/src/com/madhu/comparable/TreeSetEx.java
@@ -2,6 +2,8 @@ package com.madhu.comparable;
 
 import java.util.TreeSet;
 
+import com.madhu.newinstance.NewInstance;
+
 //Treeset() uses compareTo() method of comparable to sort the objects i
 //in natural sorting order
 public class TreeSetEx {
@@ -15,6 +17,17 @@ public class TreeSetEx {
 		//treeSet.add("madhu");//classcast exception
 		//treeSet.add(null);//nullpointer exception
 		System.out.println(treeSet);
+		TreeSet treeSet2 = new TreeSet();
+		treeSet2.add("m");
+		treeSet2.add("a");
+		treeSet2.add("d");
+		treeSet2.add("h");
+		treeSet2.add(new String("h"));
+		treeSet2.add("u");
+		//treeSet2.add(new StringBuffer("b"));
+		//classcast exception bcos StringBuffer doesn't implement 
+		//comparable
+		System.out.println(treeSet2);
 		
 
 	}
