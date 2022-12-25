8da7f37a126bd020f5c28f0614e9b364d8b28b7d
madhu <madhu.java.git@gmail.com>
2022-12-25 22:08:47 +0530

    comparator ex

--- Practice/src/com/madhu/comparable/ComparatorExx.java
+++ Practice/src/com/madhu/comparable/ComparatorExx.java
@@ -2,17 +2,24 @@ package com.madhu.comparable;
 
 import java.util.Comparator;
 import java.util.TreeSet;
-
+//sort given string and strimgbuffer objs in descending order
 import com.madhu.newinstance.NewInstance;
 
 public class ComparatorExx {
 
 	public static void main(String[] args) {
 		TreeSet treeSet= new TreeSet(new MyComparator1()) ;
-			treeSet.add(new StringBuffer("A"));
-			treeSet.add(new StringBuffer("X"));
-			treeSet.add(new StringBuffer("C"));
-			treeSet.add(new StringBuffer("S"));
+			treeSet.add(new StringBuffer("Agsds"));
+			treeSet.add("aaaa");
+			treeSet.add(new StringBuffer("Xsd"));
+			treeSet.add("aa");
+			treeSet.add("aaaaaaaaa");
+			treeSet.add(new StringBuffer("Csds"));
+			treeSet.add("aaaasdfghjdfga");
+			treeSet.add(new StringBuffer("Ssdsd"));
+			treeSet.add("a");
+			treeSet.add("aaa");
+			
 			System.out.println(treeSet);
 		
 
@@ -25,6 +32,11 @@ class MyComparator1 implements Comparator{
 		String s1= obj1.toString();
 		//String s2 = (String)obj2;//classcast exception
 		String s2 = obj2.toString();
+		if(s1.length()<s2.length())
+			return 1;
+		else if(s1.length()>s2.length())
+			return -1;
+		else
 		return -s1.compareTo(s2);
 		
 	}
