62c12cf9b3492a6a87744025faeec7dd667e87b7
madhu <madhu.java.git@gmail.com>
2022-12-27 22:43:19 +0530

    method ref with nonstatic method

--- Practice/src/com/madhu/methodreference/MethodRefEx.java
+++ Practice/src/com/madhu/methodreference/MethodRefEx.java
@@ -1,9 +1,29 @@
 package com.madhu.methodreference;
 
 public class MethodRefEx {
+//public static void m12() {
+//	for(int i=0;i<=5;i++) {
+//		System.out.println("child thread");
+//	}
+//
+//
+//}
+public  void m13() {
+	for(int i=0;i<=5;i++) {
+		System.out.println("child thread");
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+}
+	public static void main(String[] args)throws Exception {
+		//Runnable runnable = MethodRefEx::m12();
+		Runnable runnable =  new MethodRefEx()::m13;
+		Thread thread = new Thread(runnable);
+		
+		thread.start();
+		for(int i=0;i<=5;i++) {
+			System.out.println("main thread");
+		}
 
 	}
 
