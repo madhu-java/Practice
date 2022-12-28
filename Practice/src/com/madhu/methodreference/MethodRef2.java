36ecfa0032d8bb72bb1d76f0b7dd4a3a61459d39
madhu <madhu.java.git@gmail.com>
2022-12-28 21:51:15 +0530

    using lambda expression

---
+++ Practice/src/com/madhu/methodreference/MethodRef2.java
@@ -0,0 +1,20 @@
+package com.madhu.methodreference;
+interface interfacee{
+	void print(int i);
+}
+public class MethodRef2 {
+public void m1(int i) {
+	System.out.println("i:"+i*i);
+	
+}
+	public static void main(String[] args) {
+		interfacee inter1 = x->System.out.println(x);
+		inter1.print(10);
+		
+   //inter inter =  new MethodRefEx()::m1;
+interfacee inter2 = new MethodRef2()::m1;
+inter2.print(10);
+
+	}
+
+}
