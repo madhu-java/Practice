7e83f4d3d854b2ef6e473a605e6b8ff064df64c5
madhu <madhu.java.git@gmail.com>
2022-12-29 22:22:11 +0530

    consumerEx

--- Practice/src/com/madhu/consumer/ConsumerEx.java
+++ Practice/src/com/madhu/consumer/ConsumerEx.java
@@ -12,7 +12,8 @@ public class ConsumerEx {
 	public static void main(String[] args) {
 		Consumer<String> consumer = new MyConsumer();
 		consumer.accept("Hello");
- 
+		Consumer<Integer> consumer1 = i->System.out.println(i*i);
+        consumer1.accept(10);
 	}
 
 }
