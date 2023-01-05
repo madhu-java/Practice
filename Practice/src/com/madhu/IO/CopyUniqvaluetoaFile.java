45f0c0cd0062f77bd29258db8ba5ea0203b7465d
madhu <madhu.java.git@gmail.com>
2023-01-05 21:26:15 +0530

    only copy the unique ph numbers to a new file

--- Practice/src/com/madhu/IO/CopyUniqvaluetoaFile.java
+++ Practice/src/com/madhu/IO/CopyUniqvaluetoaFile.java
@@ -1,10 +1,38 @@
 package com.madhu.IO;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.sound.midi.Soundbank;
+
 public class CopyUniqvaluetoaFile {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		PrintWriter printWriter = new PrintWriter("file7.txt");
+		
+   BufferedReader bufferedReader = new BufferedReader(new FileReader("file6.txt"));
+   String target = bufferedReader.readLine();
+   boolean targetExists = false;
+   while(target!=null) {
+	   BufferedReader bufferedReader2 = new BufferedReader(new FileReader("file7.txt"));
+	   String lineString = bufferedReader2.readLine();
+	   System.out.println(lineString);
+	   while(lineString!=null) {
+	   if(target==lineString) {
+		   targetExists= true;
+		   break;
+	   }
+	   lineString =bufferedReader2.readLine();
+	   }
+	   if(targetExists==false) {
+		   printWriter.println(target);
+	   }
+	   target = bufferedReader.readLine();
+   }
+		
 	}
 
 }
